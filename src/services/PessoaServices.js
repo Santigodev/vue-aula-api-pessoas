33806614f586add5d08061fbc142dee71b5a2caf
Santigodev <shilton.santiago@gmail.com>
2024-09-27 20:56:22 -0300

    Criando projeto vue.js

---
+++ src/services/PessoaServices.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:7046/api/pessoas';
+
+export default {
+    listarTodasPessoas()
+    {
+        return axios.get(`${API_URL}/listar`);
+    },
+    deletarPessoaId(id)
+    {
+        return axios.delete(`${API_URL}/${id}`);
+    },
+    editarPessoa(id, pessoa)
+    {
+        return axios.put(`${API_URL}/${id}`,pessoa);
+    },
+    salvarPessoa(pessoa)
+    {
+        return axios.post(`${API_URL}`, pessoa);
+    }
+}
